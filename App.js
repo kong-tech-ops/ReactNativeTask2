fa3ce38edeed8f0957187461044361dd6ec45f26
SASO <saso.honkanen@gmail.com>
2022-07-12 22:23:04 +0300

    Alert base

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-import {Button, FlatList, StyleSheet, Text, TextInput, View,} from 'react-native';
+import {Button, FlatList, StyleSheet, Text, TextInput, View, Alert} from 'react-native';
 
 const App = () => {
+
   return (
         <View style={styles.container}>
 
@@ -12,16 +13,43 @@ const App = () => {
             <Button title='ADD'/>
           </View>
 
+          {/* Temp button to call alert function for now until the longpress of a flatlist functionality is done: */}
+          <View style={styles.tempButtonWrapper}>
+            <Button title='Test the Alert' onPress={CreateAlert}/>
+          </View>
+          {/* Temp button code ends*/}
+
+          
+
           <View style={styles.flatListWrapper}>
 
             <FlatList>
 
             </FlatList>
-            
+
           </View>
          
         </View>
   );
+};
+
+const CreateAlert = () => {
+  Alert.alert(
+    "Delete?",
+    "Do you want to delete this item?",
+    [
+      {
+        text: "Archive",
+        onPress: () => console.log("Archive pressed")
+      },
+      {
+        text: "Cancel",
+        onPress: () => console.log("Cancel Pressed")
+      },
+      { text: "OK", onPress: () => console.log("OK Pressed") }
+    ]
+  );
+        
 };
 
 const styles = StyleSheet.create({
@@ -45,6 +73,12 @@ const styles = StyleSheet.create({
     top: 32,
     right: 40,
     width: 80,
+  },
+  tempButtonWrapper:{
+    position: 'absolute',
+    top: 90,
+    right: 20,
+    width: 100,
   }
 });
 
