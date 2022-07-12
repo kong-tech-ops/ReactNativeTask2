e3691c7ea45bdf2c44d08faa3f3e970cacfb0fa1
VPlaami <veikka.plaami@outlook.com>
2022-07-12 22:13:40 +0300

    bugfixes

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ const App = () => {
   return (
         <View style={styles.container}>
 
-          <TextInput style={styles.textInput}>  </TextInput>
+          <TextInput style={styles.textInput}/>
 
           <View style={styles.buttonWrapper}>
             <Button title='ADD'/>
